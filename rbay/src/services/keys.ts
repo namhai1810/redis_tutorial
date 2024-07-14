053fae62490e6f3d1e15630db833f98d31399d47
namhai1810 <nnh18102003@gmail.com>
2024-07-14 17:12:26 +0700

    Feat: save and get session, serialize and deserialize

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -1,2 +1,3 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userid : string) => `users#${userid}`;
+export const sessionKey = (sessionId: string) => `session#${sessionId}`;
