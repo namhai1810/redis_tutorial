b87832ece255697318b82dd7a5a68eeea2e58734
namhai1810 <nnh18102003@gmail.com>
2024-07-14 11:14:21 +0700

    docs: Hash in redis definition and update docs in rbook

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -1,5 +1,25 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    // if exits => delete else wrong type
+    await client.del('car');
+    // after save to redis, node redis convert them to string
+    // {cylinders: 4} => '{"cylinders":4}' (String object)
+    // null and undefined cannot converted to string
+    await client.hSet('car', {
+        color: 'red',
+        year: 2020,
+        engine: {cylinders: 4},
+        owner: null || '',
+        service: undefined || '',
+    })
+    // HSET car color red year 2020
+    const car = await client.hGetAll('car#fdfdsf');
+    if (Object.keys(car).length === 0){
+        console.log('Car not found');
+        return;
+    }
+    console.log(car);
+};
 run();
