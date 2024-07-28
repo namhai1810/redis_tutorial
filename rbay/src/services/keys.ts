199c2c82f0fbbe52380c3e8da1bffe90d44ce1ba
namhai1810 <nnh18102003@gmail.com>
2024-07-28 22:34:46 +0700

    Feat: add check and save  name exist in redis in redis, and liked people

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -1,4 +1,6 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
-export const usersKey = (userid : string) => `users#${userid}`;
+export const usersKey = (userid: string) => `users#${userid}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
 export const itemKey = (itemId: string) => `item:${itemId}`;
+export const usernameUniqueKey = () => `username:unique`;
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -2,8 +2,11 @@ import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import {client} from '$services/redis';
 import {usersKey} from '$services/keys';
+import { usernameUniqueKey } from '$services/keys';
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+
+};
 
 export const getUserById = async (id: string) => {
     const user = await client.hGetAll(usersKey(id));
@@ -12,7 +15,14 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
+    // See if user is already in the database
+    // if so throw an error
+    const exists =  await client.sIsMember(usernameUniqueKey(), attrs.username);
+    if (exists) {
+        throw new Error('Username already exists');
+    }
     await client.hSet(usersKey(id), serialize(attrs));
+    await client.sAdd(usernameUniqueKey(), attrs.username);
     return id;
 };
 
